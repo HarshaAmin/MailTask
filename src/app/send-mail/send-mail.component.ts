85cb7e4cf40ba10158224ccb560410af07a1c614
sanathshetty-novigo <ss251974@ncr.com>
2024-12-30 12:49:10 +0530

    ui files

--- src/app/send-mail/send-mail.component.ts
+++ src/app/send-mail/send-mail.component.ts
@@ -1,4 +1,4 @@
-import { AfterViewChecked, AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AfterViewInit, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import $ from 'jquery';
 import { FileUploadComponent } from '../file-upload/file-upload.component';
@@ -7,24 +7,7 @@ import { SalesforceService } from '../../shared/services/salesforce.service';
 import { CommonService } from '../../shared/services/common.service';
 import { NgIf } from '@angular/common';
 import { EmojiService } from '../../shared/services/emoji.service';
-import Quill, { Delta } from 'quill';
-import { marked } from "marked";
-import TurndownService from 'turndown';
-
-const InlineSuggestion = Quill.import('blots/inline') as any;
-
-class InlineSuggestionBlot extends InlineSuggestion {
-  static blotName = 'inline-suggestion'; // Custom name for the blot
-  static tagName = 'span'; // Span tag for inline suggestion
-
-  constructor(domNode: any) {
-    super(domNode);
-    domNode.classList.add('inline-suggestion'); // Add the custom class to the element
-  }
-}
-
-// Register the custom blot with Quill
-Quill.register(InlineSuggestionBlot);
+import Quill from 'quill';
 
 @Component({
   selector: 'app-send-mail',
@@ -32,9 +15,11 @@ Quill.register(InlineSuggestionBlot);
   imports: [FormsModule, FileUploadComponent, NgIf],
   templateUrl: './send-mail.component.html'
 })
-export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecked {
+export class SendMailComponent implements OnInit, AfterViewInit {
   email = { to: '', subject: '', body: '' };
+  inlineSuggestion: string | null = null;
   files: NgxFileDropEntry[] = [];
+  suggestions: string[] = [];
   currentHighScoreSuggestion: string = '';
   cursorPosition: number = 0;
   suggestionPosition: { top: number; left: number } | null = null;
@@ -42,35 +27,6 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
   emojiPickerVisible = false;
   selectedEmoji: string = '';
   popupPosition: any = {};
-  turndownService: TurndownService;
-  isComposeMode: boolean = false;
-  quill: any;
-  isEditorInitialized = false; // Flag to track if the editor has already been initialized
-  isAddingSuggestions = false;
-  suggestions: { token_str: string; sequence: string }[] = [];
-  inlineSuggestion: { token_str: string; sequence: string } = {
-    token_str: '',
-    sequence: ''
-  };
-  suggestionTimeout: any;
-  isSuggestionVisible = false;
-  private lastAnalyzedText = '';
-  mockSuggestionsResponse = [
-    {
-      token_str: 'died',
-      sequence: "{ inputs : ' hi, i wanted to inform you that d died. ' }"
-    },
-    {
-      token_str: 'is',
-      sequence: "{ inputs : ' hi, i wanted to inform you that d is. ' }"
-    },
-    {
-      token_str: 'amazing',
-      sequence: "{ inputs : ' hi, i wanted to inform you that is amazing. ' }"
-    }
-    // Add more mock data as needed
-  ];
-  resetTimeout: any;
 
   @Output() openEmailModalEmitter = new EventEmitter<boolean>(true);
   @Input() openSendEmailModal: boolean = false;
@@ -81,8 +37,6 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
     private emojiService: EmojiService
   ) {
     document.addEventListener('keydown', this.handleEscapeKey.bind(this));
-    this.turndownService = new TurndownService();
-
   }
 
   ngOnInit(): void {
@@ -96,32 +50,14 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
     });
   }
 
-  ngAfterViewInit() {
-    if (this.isComposeMode) {
-      this.initializeEditor();
-    }
+  handleKeyDown(e: Event) {
+    console.log(e)
+    this.email.body = e.target["innerHTML"];
   }
 
-  ngAfterViewChecked() {
-    const editorElement = document.getElementById('editor');
-    if (editorElement && !this.quill) {
-      this.initializeEditor();
-    }
-  }
-
-  initializeEditor() {
-    const editorElement = document.getElementById('editor');
-    if (!editorElement) {
-      console.error('Editor element not found!');
-      return;
-    }
-
-    if (this.isEditorInitialized) {
-      return; // If editor is already initialized, don't reinitialize or bind again
-    }
-
-    this.quill = new Quill(editorElement, {
-      theme: 'snow',
+  ngAfterViewInit(): void {
+    const quill = new Quill("#editor", {
+      theme: "snow",
       modules: {
         toolbar: [
           [{ header: '1' }, { header: '2' }, { font: [] }],
@@ -133,132 +69,6 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
         ]
       }
     });
-
-    // Focus on the editor
-    this.quill.focus();
-
-    // Add listener for text changes (bind this only once)
-    this.quill.on('text-change', this.onTextChange.bind(this));
-
-    // Add event listeners for Tab and Enter keys
-    this.quill.root.addEventListener('keydown', (event: KeyboardEvent) =>
-      this.onKeyDown(event)
-    );
-
-    // Set the flag indicating the editor is initialized
-    this.isEditorInitialized = true;
-  }
-
-  onKeyDown(event: KeyboardEvent) {
-    console.log('inside onkeydown ' + this.suggestions);
-    if (event.key === 'Tab' || event.key === 'Enter') {
-      event.preventDefault(); // Prevent the default behavior (like adding a tab or new line)
-      this.insertSuggestion(); // Call your suggestion insertion method
-    }
-  }
-
-  insertSuggestion() {
-    console.log('inside insertSuggestion suggestion is  ' + this.suggestions);
-    console.log('inside insertSuggestion');
-    // Ensure a suggestion is available
-    if (!this.suggestions.length) {
-      return;
-    }
-
-    // Get the first suggestion (or modify this logic to handle multiple suggestions)
-    const suggestion = this.suggestions[0];
-
-    if (!suggestion) {
-      return;
-    }
-
-    // Insert the suggestion text at the current cursor position
-    const cursorPosition = this.quill.getSelection()?.index || 0;
-
-    // Insert the suggestion token as inline text
-    this.quill.insertText(
-      cursorPosition,
-      suggestion.token_str,
-      'inline-suggestion'
-    );
-
-    // Move the cursor after the inserted suggestion
-    this.quill.setSelection(cursorPosition + suggestion.token_str.length);
-
-    // Optionally, you can remove or update the suggestion list after applying it
-    this.suggestions = []; // Clear suggestions once applied (or modify as needed)
-  }
-
-  onTextChange(delta: Delta, oldDelta: Delta, source: string) {
-    console.log('onTextChange ');
-    // Get the raw HTML content
-    const htmlContent = this.quill.root.innerHTML;
-    console.log('onTextChange htmlContent ' + htmlContent);
-    // Use a DOM parser to parse the HTML
-    const parser = new DOMParser();
-    const doc = parser.parseFromString(htmlContent, 'text/html');
-
-    // Check if the body is not null before accessing its textContent
-    const body = doc.body;
-    if (body) {
-      // Remove any inline suggestions (or placeholders)
-      // Example: Remove all spans with a specific class used for suggestions
-      const suggestionElements = body.querySelectorAll('.suggestion-class'); // Change this selector based on your actual class
-      suggestionElements.forEach((el) => {
-        el.remove(); // Remove the suggestion element
-      });
-
-      // Now get the text without suggestions
-      const cleanedText = body.textContent?.trim() || ''; // Handle case when textContent is null
-      console.log('Cleaned text: ' + cleanedText);
-
-      // Prevent recursion if suggestions are being added
-      if (this.isAddingSuggestions || !cleanedText) {
-        return;
-      }
-
-      this.analyzeText(cleanedText);
-    } else {
-      console.error('Error: Body element is null');
-    }
-  }
-
-  addSuggestionsToEditor(
-    suggestions: { token_str: string; sequence: string }[]
-  ) {
-    this.isAddingSuggestions = true; // Prevent recursion while adding suggestions
-
-    const { token_str, sequence } = suggestions[0];
-    const start = sequence.indexOf(token_str);
-    const end = start + token_str.length;
-
-    // Insert the suggestion text as grayed-out (inline-suggestion)
-    this.quill.insertText(start, token_str, 'inline-suggestion');
-
-    // Move the cursor after the inserted suggestion
-    this.quill.setSelection(end, end);
-
-    this.isAddingSuggestions = false; // Re-enable text-change event listener
-
-    // Set timeout to hide the suggestion after 5 seconds
-    this.hideSuggestionAfterTimeout();
-  }
-
-  hideSuggestionAfterTimeout() {
-    if (this.suggestionTimeout) {
-      clearTimeout(this.suggestionTimeout); // Clear any previous timeout
-    }
-
-    this.suggestionTimeout = setTimeout(() => {
-      // Remove the suggestion (or hide it in the editor)
-      this.suggestions = []; // Reset suggestions
-      this.isSuggestionVisible = false;
-    }, 5000); // Hide suggestion after 5 seconds
-  }
-
-  convertToMarkdown(htmlContent: string): string {
-    // Using Turndown to convert HTML to markdown, removing unnecessary tags
-    return this.turndownService.turndown(htmlContent);
   }
 
   sendEmail(): void {
@@ -305,128 +115,6 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
     this.email.body = e.target['innerHTML'];
   }
 
-  async analyzeText(text: string) {
-    /*
-    try {
-      const accessToken = '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'; // Replace with your token
-      const sentimentResponse = await fetch(
-        'https://api-inference.huggingface.co/models/nlptown/bert-base-multilingual-uncased-sentiment',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${accessToken}`
-          },
-          body: JSON.stringify({ text })
-        }
-      );
-      if (!sentimentResponse.ok) {
-        throw new Error('Failed to fetch sentiment analysis');
-      }
-      const sentimentData = await sentimentResponse.json();
-      const inputData = `{ inputs: '${text} [MASK].' }`;
-      const suggestionResponse = await fetch(
-        'https://api-inference.huggingface.co/models/bert-base-uncased',
-        {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `Bearer ${accessToken}`
-          },
-          body: inputData
-        }
-      );
-      if (!suggestionResponse.ok) {
-        throw new Error('Failed to fetch suggestions');
-      }
-      const suggestionsData = await suggestionResponse.json();
-      const formattedSuggestions = suggestionsData.map((item: any) => ({
-        token_str: item.token_str.trim(), // Token text (the word or fragment to insert)
-        sequence: item.sequence // The sequence or context for the token
-      }));
-
-      // Clear old suggestions before adding new ones
-      this.clearOldSuggestions();
-
-      // Add the formatted suggestions to the editor
-      this.addSuggestionsToEditor(formattedSuggestions);
-
-      // Set the suggestion after 3 seconds
-      setTimeout(() => {
-        // Extract the first suggestion's token_str and assign it to inlineSuggestion
-        this.inlineSuggestion = this.suggestions[0]?.token_str || ''; // Only assign the token_str
-
-        // Reset the suggestion after 27 seconds if no new action occurs
-        this.resetTimeout = setTimeout(() => {
-          this.inlineSuggestion = ''; // Set suggestion to empty after 30 seconds from the start
-        }, 27000); // 27000ms = 27 seconds after setting the suggestion
-      }, 3000); // 3000ms = 3 seconds
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        console.error('Error fetching analysis:', error.message); // Safely accessing `message`
-        this.errorMessage = `Analysis failed: ${error.message}`;
-      } else {
-        console.error('Unknown error:', error); // In case it's not an instance of Error
-        this.errorMessage = `Analysis failed: An unknown error occurred.`;
-      }
-    }*/
-
-    if (text === this.lastAnalyzedText) {
-      return; // Don't analyze again if the text hasn't changed
-    }
-
-    this.lastAnalyzedText = text; // Update the last analyzed text
-    try {
-      // Mock the response to simulate the API call
-      const mockSuggestionsData = this.mockSuggestionsResponse; // Use mock response
-
-      // Format the mock response like the actual API response
-      const formattedSuggestions = mockSuggestionsData.map((item: any) => ({
-        token_str: item.token_str.trim(), // Token text (the word or fragment to insert)
-        sequence: item.sequence // The sequence or context for the token
-      }));
-
-      // Pass the mock formatted suggestions to addSuggestionsToEditor
-      this.suggestions = formattedSuggestions;
-      this.addSuggestionsToEditor(formattedSuggestions);
-
-      // Set the suggestion after 3 seconds (as in your original code)
-      setTimeout(() => {
-        this.inlineSuggestion = this.suggestions[0]; // Set the first suggestion or empty string
-
-        // Reset the suggestion after 27 seconds if no new action occurs
-        this.resetTimeout = setTimeout(() => {
-          //this.inlineSuggestion = ''; // Set suggestion to empty after 30 seconds
-        }, 27000); // 27000ms = 27 seconds after setting the suggestion
-      }, 3000); // 3000ms = 3 seconds
-    } catch (error) {
-      console.error('Error fetching analysis:', error);
-    }
-  }
-
-  convertMarkdownToHtml(markdown: string): string {
-    let html: string = '';
-
-    // Check if the result of marked is a Promise
-    const result = marked(markdown);
-
-    // If result is a promise, we need to resolve it
-    if (result instanceof Promise) {
-      result
-        .then((resolvedHtml: string) => {
-          html = resolvedHtml; // Handle resolved value
-        })
-        .catch((error: any) => {
-          console.error('Error converting markdown to HTML:', error);
-        });
-    } else {
-      // If it's a string, use it directly
-      html = result;
-    }
-
-    return html;
-  }
-
   handleKeyPress(e: Event) {
     const textarea = document.getElementById('bodyPreview');
     const cursorPos = textarea['selectionStart'];
@@ -451,6 +139,37 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
     document.execCommand('underline');
   }
 
+  onContentEditableInput(event: Event) {
+    this.inlineSuggestion = '';
+    //uncomment code 136
+    // const target = event.target as HTMLElement;
+    // this.emailtoSend.bodyPreview = target.innerHTML.trim();
+    // const inputText = this.emailtoSend.bodyPreview;
+    console.log('inside onContentedit');
+    const target = event.target as HTMLElement;
+    const sanitizedText = this.sanitizeInput(target.innerHTML.trim());
+    //const inputText = this.emailtoSend.bodyPreview;
+
+    // Clear previous analysis if no input
+    if (sanitizedText.length === 0) {
+      this.inlineSuggestion = '';
+      return;
+    }
+
+    // Adjust suggestion position with offset for normal typing
+    const selection = window.getSelection();
+    const rect = selection?.getRangeAt(0).getBoundingClientRect();
+
+    if (rect) {
+      this.suggestionPosition = {
+        top: rect.bottom + window.scrollY, // 5px offset for regular input
+        left: rect.left + window.scrollX
+      };
+      console.log(this.suggestionPosition);
+    }
+    console.log('inside onContentedit 170');
+    this.inlineSuggestion = this.getRandomSuggestion();
+  }
 
   getRandomSuggestion() {
     const dummySuggestions = [
@@ -544,6 +263,83 @@ export class SendMailComponent implements OnInit, AfterViewInit, AfterViewChecke
     console.log('Sanitized Text:', sanitizedText); // Log the final sanitized text
 
     return sanitizedText;
+  }
+
+  onKeyDown(event: KeyboardEvent) {
+    if (event.key === 'Tab' && this.inlineSuggestion) {
+      event.preventDefault(); // Stop default behavior (Tab behavior)
+
+      // Get the target element and sanitize its HTML content
+      const target = event.target as HTMLElement;
+      console.log('target.innerHTML.trim() before ' + target.innerHTML.trim());
+      const sanitizedText = this.sanitizeInput(target.innerHTML.trim());
+
+      console.log('sanitizedText after ' + sanitizedText);
+
+      const textBefore = sanitizedText;
+      const { row, column } = this.caretPosition;
+      const lines = textBefore.split('\n');
+      console.log('row in final is ' + row);
+      console.log('lines in final is ' + JSON.stringify(lines));
+      console.log('lines length is ' + lines.length);
+
+      // Adjust row based on the caret position
+      let lineRow = 0;
+
+      // Ensure row is within bounds (preventing it from being negative or out of bounds)
+      if (row < 0) {
+        lineRow = 0; // If row is negative, default to the first line
+      } else if (row >= lines.length) {
+        lineRow = lines.length - 1; // If row exceeds the available lines, use the last line
+      } else {
+        lineRow = row; // Otherwise, use the row as it is
+      }
+
+      // Update the line where the suggestion will be inserted
+      if (lineRow >= 0 && lineRow < lines.length) {
+        const currentLine = lines[lineRow];
+        const beforeCaret = currentLine.substring(0, column); // Text before the caret
+        const afterCaret = currentLine.substring(column); // Text after the caret
+
+        // Insert the suggestion text in the correct position
+        lines[lineRow] =
+          beforeCaret + ' ' + this.inlineSuggestion + ' ' + afterCaret;
+      }
+
+      let result = lines.map(
+        (str) =>
+          str
+            .replaceAll('replace_as_bold_start', '<b>') // Replace start bold marker with <b>
+            .replaceAll('replace_as_bold_end', '</b>') // Replace end bold marker with </b>
+      );
+
+      // Join lines back and log the updated text
+      const updatedText = result.join('\n');
+      console.log('Updated Text with Suggestion:', updatedText);
+
+      const formattedHTML = result
+        .map((line) => {
+          // For empty lines, return <div><br></div>
+          return line.trim() === '' ? '<div><br></div>' : `<div>${line}</div>`;
+        })
+        .join('');
+
+      // Update the target innerHTML with the new lines
+      target.innerHTML = formattedHTML;
+      // target.innerHTML = lines
+      //   .map((line) => {
+      //     // Preserve empty lines by checking explicitly for empty strings
+      //     return line.trim() === '' ? '<div><br></div>' : `<div>${line}</div>`;
+      //   })
+      //   .join('');
+
+      // Reset inline suggestion and suggestions array
+      this.inlineSuggestion = '';
+      this.suggestions = [];
+
+      // Move caret to the end of the inserted text
+      this.moveCaretToEnd(target);
+    }
   }
 
   moveCaretToEnd(target: HTMLElement) {
