c93f5a4726e81c537ae42c63161a1ad53f61e16c
sanathshetty-novigo <ss251974@ncr.com>
2024-12-30 14:24:25 +0530

    ui files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: '', component: HomeComponent }
+  { path: 'home', component: HomeComponent }
 ];
