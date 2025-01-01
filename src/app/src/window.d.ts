9b3fba102549c61715d8fd43be8e9613d4d2d02e
HarshaAmin <harshaamin53@gmail.com>
2025-01-01 15:59:13 +0530

    Delete src directory

--- src/app/src/window.d.ts
+++
@@ -1,8 +0,0 @@
-// window.d.ts or custom.d.ts (or any other name you choose)
-declare global {
-  interface Window {
-    embedded_svc: any; // You can replace 'any' with a more specific type if desired
-  }
-}
-
-export {};
